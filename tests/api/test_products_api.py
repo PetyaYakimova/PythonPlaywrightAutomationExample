333ca82497c9d9edb5936d34083cdb1011ce1394
Petya Yakimova <petya_yakimova@abv.bg>
2026-01-14 08:56:19 +0200

    Add product list test

--- tests/api/test_products_api.py
+++ tests/api/test_products_api.py
@@ -19,3 +19,19 @@ def test_get_all_products(api_client):
         schema = json.load(f)
 
     validate(instance=data, schema=schema)
+
+
+@pytest.mark.api
+def test_products_have_required_fields(api_client):
+    response = api_client.get("/productsList")
+    assert response.status_code == 200
+
+    products = response.json()["products"]
+
+    # Validate structure of the first product
+    product = products[0]
+
+    required_fields = ["id", "name", "price", "brand", "category"]
+
+    for field in required_fields:
+        assert field in product
