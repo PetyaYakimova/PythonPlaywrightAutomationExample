ccbaf9f8955f818b68e45eb69414d22132fee41b
Petya Yakimova <petya_yakimova@abv.bg>
2026-01-16 08:42:28 +0200

    Add test for search products

--- tests/api/test_search_api.py
+++ tests/api/test_search_api.py
@@ -25,3 +25,16 @@ def test_search_product_without_payload(api_client):
 
     assert response.status_code == 200
     assert "products" not in response.json()
+
+
+@pytest.mark.api
+def test_search_returns_relevant_products(api_client):
+    search_term = "panda"
+
+    response = api_client.post("/searchProduct", data={"search_product": search_term})
+    assert response.status_code == 200
+
+    products = response.json()["products"]
+
+    for product in products:
+        assert search_term.lower() in product["name"].lower()
