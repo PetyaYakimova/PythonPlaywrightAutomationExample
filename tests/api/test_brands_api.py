61a9acc6daf6da2313359edaccd3c520604eee95
Petya Yakimova <petya_yakimova@abv.bg>
2026-01-16 08:43:21 +0200

    Add brand test

--- tests/api/test_brands_api.py
+++ tests/api/test_brands_api.py
@@ -10,3 +10,15 @@ def test_get_all_brands(api_client):
     data = response.json()
     assert "brands" in data
     assert len(data["brands"]) > 0
+
+
+@pytest.mark.api
+def test_brands_in_products_exist_in_brands_list(api_client):
+    brands_response = api_client.get("/brandsList")
+    product_response = api_client.get("/productsList")
+
+    brands = {b["brand"] for b in brands_response.json()["brands"]}
+    products = product_response.json()["products"]
+
+    for product in products:
+        assert product["brand"] in brands
