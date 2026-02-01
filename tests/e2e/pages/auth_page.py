0da4ca6bc4eb9f6bc2e1345716d1e74b27b88404
Petya Yakimova <petya_yakimova@abv.bg>
2026-02-01 10:20:50 +0200

    Add auth page

---
+++ tests/e2e/pages/auth_page.py
@@ -0,0 +1,18 @@
+from .base_page import BasePage
+
+
+class AuthPage(BasePage):
+    def open(self):
+        self.page.goto("https://automationexercise.com/login")
+
+    def start_signup(self, name, email):
+        self.fill("input[data-qa='signup-name']", name)
+        self.fill("input[data-qa='signup-email']", email)
+        self.click("button[data-qa='signup-button']")
+
+    def complete_signup(self, password):
+        self.fill("input[data-qa='password']", password)
+        self.click("button[data-qa='create-account']")
+
+    def success_message_visible(self):
+        return self.is_visible("text=Account Created!")
