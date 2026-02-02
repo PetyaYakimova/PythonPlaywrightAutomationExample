85da4a629a1cf06621e8e188e6c67318d440b20b
Petya Yakimova <petya_yakimova@abv.bg>
2026-02-02 08:34:32 +0200

    Move logic from auth page to login page

--- tests/e2e/pages/login_page.py
+++ tests/e2e/pages/login_page.py
@@ -12,3 +12,15 @@ class LoginPage(BasePage):
 
     def error_visible(self):
         return self.is_visible("text=Your email or password is incorrect!")
+
+    def start_signup(self, name, email):
+        self.fill("input[data-qa='signup-name']", name)
+        self.fill("input[data-qa='signup-email']", email)
+        self.click("button[data-qa='signup-button']")
+
+    def complete_signup(self, password):
+        self.fill("input[data-qa='password']", password)
+        self.click("button[data-qa='create-account']")
+
+    def success_message_visible(self):
+        return self.is_visible("text=Account Created!")
