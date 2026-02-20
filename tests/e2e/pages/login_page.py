1c4f71ea015c1517ca7a5381a2327f1d6f61e2f7
Petya Yakimova <petya_yakimova@abv.bg>
2026-02-20 08:21:22 +0200

    Update the complete signup method

--- tests/e2e/pages/login_page.py
+++ tests/e2e/pages/login_page.py
@@ -18,8 +18,30 @@ class LoginPage(BasePage):
         self.fill("input[data-qa='signup-email']", email)
         self.click("button[data-qa='signup-button']")
 
-    def complete_signup(self, password):
-        self.fill("input[data-qa='password']", password)
+    def complete_signup(self, data):
+        # Title
+        self.page.click("input#id_gender1")
+
+        # Password
+        self.fill("input[data-qa='password']", data["password"])
+
+        # Date of Birth (static values for stability)
+        self.page.select_option("#days", "1")
+        self.page.select_option("#months", "1")
+        self.page.select_option("#years", "2000")
+
+        # Address Information
+        self.fill("input[data-qa='first_name']", data["first_name"])
+        self.fill("input[data-qa='last_name']", data["last_name"])
+        self.fill("input[data-qa='address']", data["address"])
+
+        self.page.select_option("select[data-qa='country']", data["country"])
+
+        self.fill("input[data-qa='state']", data["state"])
+        self.fill("input[data-qa='city']", data["city"])
+        self.fill("input[data-qa='zipcode']", data["zipcode"])
+        self.fill("input[data-qa='mobile_number']", data["mobile"])
+
         self.click("button[data-qa='create-account']")
 
     def success_message_visible(self):
