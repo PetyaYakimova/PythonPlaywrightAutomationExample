27a84f3904fbde7a72c79d8c554802446f5603c7
Petya Yakimova <petya_yakimova@abv.bg>
2026-02-24 08:51:54 +0200

    Change open product details page

--- tests/e2e/test_product_details_e2e.py
+++ tests/e2e/test_product_details_e2e.py
@@ -1,14 +1,14 @@
 import time
-
 import pytest
+from playwright.sync_api import expect
 
 
 @pytest.mark.e2e
 def test_open_product_details(page):
     page.goto("https://automationexercise.com/products")
 
-    # Click first product "View Product"
-    page.click("text=View Product")
+    # Click first "View Product" link safely
+    page.locator("a[href*='/product_details/']").first.click()
 
     # Assert product details visible - need to fix this the first assertion doesn't pass
     assert page.locator("text=Availability").is_visible()
