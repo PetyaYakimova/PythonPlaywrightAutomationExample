aa3cf9e783337761b6fd80462d6756ad5bf772b0
Petya Yakimova <petya_yakimova@abv.bg>
2026-03-10 08:22:32 +0200

    Add cart page in test

--- tests/e2e/test_checkout_guest_e2e.py
+++ tests/e2e/test_checkout_guest_e2e.py
@@ -1,6 +1,7 @@
 import pytest
 from playwright.sync_api import expect
 from tests.e2e.pages.products_page import ProductsPage
+from tests.e2e.pages.cart_page import CartPage
 
 
 # TODO: Update it to use the cart page
@@ -12,6 +13,7 @@ def test_guest_can_reach_checkout(page):
     products.add_first_product_to_cart()
     products.view_cart()
 
+    cartPage = CartPage(page)
     page.click("text=Proceed To Checkout")
 
     expect(page.get_by_role("heading", name="Checkout")).to_be_visible()
