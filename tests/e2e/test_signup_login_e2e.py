2d51b0dc8a1a880c80b528e2265dc0117ca056d2
Petya Yakimova <petya_yakimova@abv.bg>
2026-03-20 07:58:08 +0200

    Update login test

--- tests/e2e/test_signup_login_e2e.py
+++ tests/e2e/test_signup_login_e2e.py
@@ -17,7 +17,7 @@ def test_user_can_signup_and_login(page):
     expect(login_page.account_created_message_locator()).to_be_visible()
 
     login_page.click_continue_button()
-    expect(page.locator("text=Logged in as")).to_be_visible()
+    expect(login_page.logged_in_message_locator()).to_be_visible()
 
 
 @pytest.mark.e2e
