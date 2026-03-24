4f5452cf7d314bea19012319516c850a0453dc47
Petya Yakimova <petya_yakimova@abv.bg>
2026-03-24 08:47:50 +0200

    Update variable name

--- tests/e2e/test_category_filter_e2e.py
+++ tests/e2e/test_category_filter_e2e.py
@@ -4,10 +4,10 @@ from tests.e2e.pages.products_page import ProductsPage
 
 @pytest.mark.e2e
 def test_filter_by_category(page):
-    products = ProductsPage(page)
-    products.open()
+    products_page = ProductsPage(page)
+    products_page.open()
 
-    products.click_filter("Women")
-    products.click_filter("Dress")
+    products_page.click_filter("Women")
+    products_page.click_filter("Dress")
 
-    assert products.results_visible()
+    assert products_page.results_visible()
