9423e6bf7a397d4f55832c6840956a339dd6e586
Petya Yakimova <petya_yakimova@abv.bg>
2026-03-24 08:47:18 +0200

    Update cart method

--- tests/e2e/pages/cart_page.py
+++ tests/e2e/pages/cart_page.py
@@ -8,11 +8,11 @@ class CartPage(BasePage):
     def remove_first_item(self):
         return self.page.click(".cart_quantity_delete")
 
-    def has_items(self):
-        return self.page.locator(".cart_product").count() > 0
-
     def item_in_cart_locator(self):
         return self.page.locator(".cart_product")
+
+    def has_items(self):
+        return self.item_in_cart_locator().count() > 0
 
     def proceed_to_checkout(self):
         self.click("text=Proceed To Checkout")
