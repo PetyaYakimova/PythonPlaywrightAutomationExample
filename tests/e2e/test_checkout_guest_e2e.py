88a991d4308d99ced9fbce4d67394234b3c08114
Petya Yakimova <petya_yakimova@abv.bg>
2026-03-25 08:26:26 +0200

    Update checkout test

--- tests/e2e/test_checkout_guest_e2e.py
+++ tests/e2e/test_checkout_guest_e2e.py
@@ -2,6 +2,7 @@ import pytest
 from playwright.sync_api import expect
 from tests.e2e.pages.products_page import ProductsPage
 from tests.e2e.pages.cart_page import CartPage
+from tests.e2e.pages.checkout_page import CheckoutPage
 
 
 @pytest.mark.e2e
@@ -15,4 +16,5 @@ def test_guest_can_reach_checkout(page):
     cart_page = CartPage(page)
     cart_page.proceed_to_checkout()
 
-    expect(page.get_by_role("heading", name="Checkout")).to_be_visible()
+    checkout_page = CheckoutPage(page)
+    expect(checkout_page.checkout_heading_locator()).to_be_visible()
