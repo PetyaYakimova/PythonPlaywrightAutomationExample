ca667a1fb3ef75a14ab15a59562b80e0fdf91083
Petya Yakimova <petya_yakimova@abv.bg>
2026-04-08 08:46:59 +0300

    Extract variable in search test

--- tests/e2e/test_search_e2e.py
+++ tests/e2e/test_search_e2e.py
@@ -5,9 +5,11 @@ from tests.e2e.pages.products_page import ProductsPage
 
 @pytest.mark.e2e
 def test_search_product(page):
+    keyword = 'Top'
+
     products_page = ProductsPage(page)
     products_page.open()
 
-    products_page.search("Top")
+    products_page.search(keyword)
 
-    expect(products_page.get_first_product_description_locator()).to_contain_text('Top')
+    expect(products_page.get_first_product_description_locator()).to_contain_text(keyword)
